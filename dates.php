6dc28ce2f0844418b3c6b88708dc95da25b43a94
manojdhiman <manoj@rudrainnovatives.com>
2014-10-13 10:44:03 +0530

    Update dates.php

--- dates.php
+++ dates.php
@@ -35,4 +35,20 @@ $period   = new DatePeriod($start, $interval, $end);
 foreach ($period as $dt) {
     echo $dt->format("Y-m-d") . "<br>\n";
 }
+
+
+//// get the weeks date 
+$p = new DatePeriod(
+		    new DateTime($curr_year.'-'.$curr_month.'-01'), 
+		    new DateInterval('P1W'), 
+		    new DateTime($curr_year2.'-'.$curr_month2.'-01')
+			);
+		foreach ($p as $w) {
+		    $date_week= $w->format('Y-m-d');
+		    $date_week_7= strtotime($date_week);
+			$date_7 =date('Y-m-d',strtotime("+7 day",  $date_week_7));
+			
+		    $res2= $soil_model->get_weekly_lop_data($date_week,$date_7);
+		    array_push($weakly_res,$res2);
+		}
 ?>
